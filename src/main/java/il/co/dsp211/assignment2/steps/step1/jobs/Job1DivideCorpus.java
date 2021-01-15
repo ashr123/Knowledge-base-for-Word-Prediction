71906c35f22463bcd9bf27d4243c1fe01e7d5da4
Roy Ash <roy.ash456@gmail.com>
2021-01-15 16:24:56 +0200

    made tables
    Signed-off-by: Roy Ash <roy.ash456@gmail.com>

--- src/main/java/il/co/dsp211/assignment2/steps/step1/jobs/Job1DivideCorpus.java
+++ src/main/java/il/co/dsp211/assignment2/steps/step1/jobs/Job1DivideCorpus.java
@@ -21,12 +21,12 @@ public class Job1DivideCorpus
 {
 	public static class DividerMapper extends Mapper<LongWritable, Text, Text, BooleanLongPair>
 	{
-		private Pattern hebrewPattern;
+		private Pattern pattern;
 
 		@Override
 		protected void setup(Context context)
 		{
-			hebrewPattern = Pattern.compile("(?<words>" + String.join("+ ", Collections.nCopies(3, context.getConfiguration().get("singleLetterInAWordRegex"))) + "+)\\t\\d{4}\\t(?<occurrences>\\d+).*");
+			pattern = Pattern.compile("(?<words>" + String.join("+ ", Collections.nCopies(3, context.getConfiguration().get("singleLetterInAWordRegex"))) + "+)\\t\\d{4}\\t(?<occurrences>\\d+).*");
 		}
 
 		/**
@@ -37,7 +37,7 @@ public class Job1DivideCorpus
 		@Override
 		protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
 		{
-			final Matcher matcher = hebrewPattern.matcher(value.toString());
+			final Matcher matcher = pattern.matcher(value.toString());
 			if (matcher.matches())
 				context.write(new Text(matcher.group("words")), new BooleanLongPair(key.get() % 2 == 0, Long.parseLong(matcher.group("occurrences"))));
 		}
